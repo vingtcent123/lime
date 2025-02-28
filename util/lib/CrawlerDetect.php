46da51924aa1d61c77e760993a7cb628d62d2f0a
Vincent Guth <vincent.guth@gmail.com>
2025-02-28 11:17:38 +0100

    Affichage de toutes les interventions

--- util/lib/CrawlerDetect.php
+++ util/lib/CrawlerDetect.php
@@ -22,141 +22,140 @@ use Jaybizzle\CrawlerDetect\Fixtures\Exclusions;
 
 class CrawlerDetect
 {
-    /**
-     * The user agent.
-     *
-     * @var null
-     */
-    protected $userAgent = null;
+	/**
+	 * The user agent.
+	 *
+	 * @var string|null
+	 */
+	protected $userAgent;
 
-    /**
-     * Headers that contain a user agent.
-     *
-     * @var array
-     */
-    protected $httpHeaders = array();
+	/**
+	 * Headers that contain a user agent.
+	 *
+	 * @var array
+	 */
+	protected $httpHeaders = [];
 
-    /**
-     * Store regex matches.
-     *
-     * @var array
-     */
-    protected $matches = array();
+	/**
+	 * Store regex matches.
+	 *
+	 * @var array
+	 */
+	protected $matches = [];
 
-    /**
-     * Crawlers object.
-     *
-     * @var \Jaybizzle\CrawlerDetect\Fixtures\Crawlers
-     */
-    protected $crawlers;
+	/**
+	 * Crawlers object.
+	 *
+	 * @var \Jaybizzle\CrawlerDetect\Fixtures\Crawlers
+	 */
+	protected $crawlers;
 
-    /**
-     * Exclusions object.
-     *
-     * @var \Jaybizzle\CrawlerDetect\Fixtures\Exclusions
-     */
-    protected $exclusions;
+	/**
+	 * Exclusions object.
+	 *
+	 * @var \Jaybizzle\CrawlerDetect\Fixtures\Exclusions
+	 */
+	protected $exclusions;
 
-    /**
-     * Headers object.
-     *
-     * @var \Jaybizzle\CrawlerDetect\Fixtures\Headers
-     */
-    protected $uaHttpHeaders;
+	/**
+	 * Headers object.
+	 *
+	 * @var \Jaybizzle\CrawlerDetect\Fixtures\Headers
+	 */
+	protected $uaHttpHeaders;
 
-    /**
-     * The compiled regex string.
-     *
-     * @var string
-     */
-    protected $compiledRegex;
+	/**
+	 * The compiled regex string.
+	 *
+	 * @var string
+	 */
+	protected $compiledRegex;
 
-    /**
-     * The compiled exclusions regex string.
-     *
-     * @var string
-     */
-    protected $compiledExclusions;
+	/**
+	 * The compiled exclusions regex string.
+	 *
+	 * @var string
+	 */
+	protected $compiledExclusions;
 
-    /**
-     * Class constructor.
-     */
-    public function __construct(?array $headers = null, $userAgent = null)
-    {
-        $this->crawlers = new Crawlers();
-        $this->exclusions = new Exclusions();
-        $this->uaHttpHeaders = new Headers();
+	/**
+	 * Class constructor.
+	 */
+	public function __construct(?array $headers = null, $userAgent = null)
+	{
+		$this->crawlers = new Crawlers;
+		$this->exclusions = new Exclusions;
+		$this->uaHttpHeaders = new Headers;
 
-        $this->compiledRegex = $this->compileRegex($this->crawlers->getAll());
-        $this->compiledExclusions = $this->compileRegex($this->exclusions->getAll());
+		$this->compiledRegex = $this->compileRegex($this->crawlers->getAll());
+		$this->compiledExclusions = $this->compileRegex($this->exclusions->getAll());
 
-        $this->setHttpHeaders($headers);
-        $this->userAgent = $this->setUserAgent($userAgent);
-    }
+		$this->setHttpHeaders($headers);
+		$this->setUserAgent($userAgent);
+	}
 
-    /**
-     * Compile the regex patterns into one regex string.
-     *
-     * @param array
-     *
-     * @return string
-     */
-    public function compileRegex($patterns)
-    {
-        return '('.implode('|', $patterns).')';
-    }
+	/**
+	 * Compile the regex patterns into one regex string.
+	 *
+	 * @param array
+	 * @return string
+	 */
+	public function compileRegex($patterns)
+	{
+		return '('.implode('|', $patterns).')';
+	}
 
-    /**
-     * Set HTTP headers.
-     *
-     * @param array|null $httpHeaders
-     */
-    public function setHttpHeaders($httpHeaders)
-    {
-        // Use global _SERVER if $httpHeaders aren't defined.
-        if (! is_array($httpHeaders) || ! count($httpHeaders)) {
-            $httpHeaders = $_SERVER;
-        }
+	/**
+	 * Set HTTP headers.
+	 *
+	 * @param  array|null  $httpHeaders
+	 */
+	public function setHttpHeaders($httpHeaders)
+	{
+		// Use global _SERVER if $httpHeaders aren't defined.
+		if (! is_array($httpHeaders) || ! count($httpHeaders)) {
+			$httpHeaders = $_SERVER;
+		}
 
-        // Clear existing headers.
-        $this->httpHeaders = array();
+		// Clear existing headers.
+		$this->httpHeaders = [];
 
-        // Only save HTTP headers. In PHP land, that means
-        // only _SERVER vars that start with HTTP_.
-        foreach ($httpHeaders as $key => $value) {
-            if (strpos($key, 'HTTP_') === 0) {
-                $this->httpHeaders[$key] = $value;
-            }
-        }
-    }
+		// Only save HTTP headers. In PHP land, that means
+		// only _SERVER vars that start with HTTP_.
+		foreach ($httpHeaders as $key => $value) {
+			if (strpos($key, 'HTTP_') === 0) {
+				$this->httpHeaders[$key] = $value;
+			}
+		}
+	}
 
-    /**
-     * Return user agent headers.
-     *
-     * @return array
-     */
-    public function getUaHttpHeaders()
-    {
-        return $this->uaHttpHeaders->getAll();
-    }
+	/**
+	 * Return user agent headers.
+	 *
+	 * @return array
+	 */
+	public function getUaHttpHeaders()
+	{
+		return $this->uaHttpHeaders->getAll();
+	}
 
-    /**
-     * Set the user agent.
-     *
-     * @param string $userAgent
-     */
-    public function setUserAgent($userAgent)
-    {
-        if (is_null($userAgent)) {
-            foreach ($this->getUaHttpHeaders() as $altHeader) {
-                if (isset($this->httpHeaders[$altHeader])) {
-                    $userAgent .= $this->httpHeaders[$altHeader].' ';
-                }
-            }
-        }
+	/**
+	 * Set the user agent.
+	 *
+	 * @param  string|null  $userAgent
+	 */
+	public function setUserAgent($userAgent)
+	{
+		if (is_null($userAgent)) {
+			foreach ($this->getUaHttpHeaders() as $altHeader) {
+				if (isset($this->httpHeaders[$altHeader])) {
+					$userAgent .= $this->httpHeaders[$altHeader].' ';
+				}
+			}
+		}
 
-        return $userAgent;
-    }
+		return $this->userAgent = $userAgent;
+	}
 
 	/**
 	 * Check user agent string against the regex.
@@ -172,32 +171,30 @@ class CrawlerDetect
 			$userAgent ?: $this->userAgent ?: ''
 		));
 
-		  if($agent === NULL) {
-			  return false;
-		  }
+		if ($agent === '') {
+			$this->matches = [];
 
-        $agent = preg_replace('/'.$this->compiledExclusions.'/i', '', $agent);
+			return false;
+		}
 
-        if(strlen(trim($agent)) == 0) {
-            return false;
-        }
+		return (bool) preg_match("/{$this->compiledRegex}/i", $agent, $this->matches);
+	}
 
-        $result = preg_match('/'.$this->compiledRegex.'/i', trim($agent), $matches);
+	/**
+	 * Return the matches.
+	 *
+	 * @return string|null
+	 */
+	public function getMatches()
+	{
+		return isset($this->matches[0]) ? $this->matches[0] : null;
+	}
 
-        if ($matches) {
-            $this->matches = $matches;
-        }
-
-        return (bool) $result;
-    }
-
-    /**
-     * Return the matches.
-     *
-     * @return string|null
-     */
-    public function getMatches()
-    {
-        return isset($this->matches[0]) ? $this->matches[0] : null;
-    }
+	/**
+	 * @return string|null
+	 */
+	public function getUserAgent()
+	{
+		return $this->userAgent;
+	}
 }
